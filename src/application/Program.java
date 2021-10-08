109b6dc58ec3257abdf38b2c292fcf602b1f6f5b
Gustavo Jackson <gustajpe@gmail.com>
2021-10-08 16:10:58 -0300

    projeto xadrez

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Projeto de Xadrez - teste new");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module project_01_chess_system_game {
+}
